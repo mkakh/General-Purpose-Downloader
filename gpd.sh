35eca532945f58ab8783aab4f35144e70592547b
akh <akh@mkakh.com>
2017-10-10 22:01:29 +0900

    Parallelize

--- gpd.sh
+++ gpd.sh
@@ -7,16 +7,19 @@ if [ $# -ne 2 ]; then
     exit 1
 fi
 
+NPROC=8
 EXTENSION="$1"
-TARGET="$2"
+TARGET_SITE="$2"
 SAVEPATH="$PWD"
 
-HTML=$(curl $TARGET 2>/dev/null)
+HTML=$(curl $TARGET_SITE 2>/dev/null)
 URLS=$(echo $HTML | grep -Eo "[^\"]+${EXTENSION}")
+TARGET_FILES=""
 for URL in $URLS; do
     if [[ "$URL" =~ ^(http|https).+ ]]; then
-        wget -nc -P ${SAVEPATH} ${URL}
+        TARGET_FILES="${TARGET_FILES} ${URL}"
     else
-        wget -nc -P ${SAVEPATH} ${TARGET}/${URL}
+        TARGET_FILES="${TARGET_FILES} ${TARGET_SITE}/${URL}"
     fi
 done
+echo ${TARGET_FILES} | xargs -n 1 -P $NPROC wget -nc -P $SAVEPATH
